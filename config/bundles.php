bfec1094238ec27b87979965932657e378f505b7
reubenCOclock <chourakirgit@yahoo.com>
2020-05-25 08:36:18 +0200

    remove doctrine fixtures bundle

--- config/bundles.php
+++ config/bundles.php
@@ -14,7 +14,7 @@ return [
     //Symfony\Bundle\DebugBundle\DebugBundle::class => ['dev' => true, 'test' => true],
    // Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
    // Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
-    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
+   // Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
