33b524f697e2003b71576936cc9036b9219b08af
reubenCOclock <chourakirgit@yahoo.com>
2020-05-25 08:37:45 +0200

    remove alice bundle

--- config/bundles.php
+++ config/bundles.php
@@ -15,7 +15,7 @@ return [
    // Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
    // Symfony\Bundle\WebServerBundle\WebServerBundle::class => ['dev' => true],
    // Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
-    Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
+    //Nelmio\Alice\Bridge\Symfony\NelmioAliceBundle::class => ['dev' => true, 'test' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
     Knp\Bundle\SnappyBundle\KnpSnappyBundle::class => ['all' => true],
    
